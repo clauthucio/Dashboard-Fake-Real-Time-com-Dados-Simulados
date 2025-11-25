bf98381a56dfb22910ba602cbc76088de79bdb5e
Lucas da Silva Gomes <lucas.gomes.manaus@gmail.com>
2025-11-25 12:35:36 -0400

    :wrench: chore(setup): configure ESLint, Prettier e Husky para padronização de código
    - Adicionada configuração do ESLint com regras base do projeto
    - Integrado Prettier como formatter padrão, evitando conflitos com o ESLint
    - Criado hook do Husky para rodar lint antes dos commits
    - Padronizadas convenções de estilo, imports e formatação
    - Garantida maior consistência, legibilidade e qualidade do código

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const { defineConfig } = require("eslint/config");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: ['app', 'z'],
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: ['app', 'z'],
+          style: 'kebab-case',
+        },
+      ],
+      'prettier/prettier': ['error', { semi: false }], // Enforce Prettier formatting as lint errors
+      semi: ['off'],
+      '@typescript-eslint/no-explicit-any': 'off', // Forbid usage of 'any'
+      '@typescript-eslint/no-unused-vars': 'off', // Allow unused variables
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {},
+  },
+]);
