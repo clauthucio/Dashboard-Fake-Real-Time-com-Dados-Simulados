3836bfc99d1bd31301e8017f9bde68aa56d7d100
Lucas da Silva Gomes <lucas.gomes.manaus@gmail.com>
2025-12-10 12:44:13 -0400

    ✨ feat:  autenticação, pages básicas e rotas protegidas
    - Adiciona sistema de autenticação com armazenamento de sessão no localStorage
    - Implementa rotas protegidas com verificação de login
    - Cria páginas iniciais sem estilização: Login, Operador, Supervisor, Admin e Acesso Negado
    - Impede acessar qualquer rota sem estar autenticado
    - Adiciona controle de acesso baseado em papéis (role-based routing)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,54 @@
 import { Routes } from '@angular/router';
+import { RedirectGuard } from '../auth/redirect.guard';
+import { Login } from '../pages/login/login';
+import { AuthGuard } from '../auth/auth.guard';
+import { RoleGuard } from '../auth/role.guard';
+import { Admin } from '../pages/admin/admin';
+import { Supervisor } from '../pages/supervisor/supervisor';
+import { Operador } from '../pages/operador/operador';
+import { AcessoNegado } from '../pages/acesso-negado/acesso-negado';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // LOGIN
+  {
+    path: 'login',
+    canActivate: [RedirectGuard],
+    component: Login,
+  },
+
+  // ADMIN
+  {
+    path: 'admin',
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['admin'] },
+    component: Admin,
+  },
+
+  // SUPERVISOR
+  {
+    path: 'supervisor',
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['supervisor', 'admin'] },
+    component: Supervisor,
+  },
+
+  // OPERADOR
+  {
+    path: 'operador',
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['operador', 'supervisor', 'admin'] },
+    component: Operador,
+  },
+
+  // ACESSO NEGADO
+  {
+    path: 'acesso-negado',
+    component: AcessoNegado,
+  },
+
+  // ROOT
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // 404
+  { path: '**', redirectTo: 'login' },
+];
---
+++ src/services/api.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface LoginPayload {
+  username: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  id: number;
+  username: string;
+  name: string;
+  cargo: 'admin' | 'supervisor' | 'operador';
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api {
+  private baseUrl = 'http://localhost:3000';
+  private http = inject(HttpClient);
+
+  // getEndpointUrl(endpoint: string): string {
+  //   return `${this.baseUrl}/${endpoint}`
+  // }
+  login(payload: LoginPayload): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.baseUrl}/login`, payload);
+  }
+}
