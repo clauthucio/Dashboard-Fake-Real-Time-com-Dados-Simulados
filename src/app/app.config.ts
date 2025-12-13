f5fc75e3ba8355ff5b73042fd6807f2f0bd1957f
Lucas da Silva Gomes <lucas.gomes.manaus@gmail.com>
2025-12-13 16:11:33 -0400

    ✨ feat: chart teperature componet
    - Foi adicionado um chart componet de temperatura, para que ele seja generico, apanas passando os dados : value:number,name:string,sector:string,

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+//animações do chart @swimlane/ngx-charts@23.1.0
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -8,6 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
-  ]
+    provideHttpClient(),
+    provideAnimations(), // <--- ESSENCIAL
+  ],
 };
